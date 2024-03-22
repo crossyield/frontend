1284719b06960a08c78cc460844b5c796a6e199d
Cheong Yee Chian <cheongyeechian@gmail.com>
2024-03-23 02:29:57 +0800

    add deposit, borrow, withdraw

--- src/app/dashboard/components/Card.tsx
+++ src/app/dashboard/components/Card.tsx
@@ -2,19 +2,26 @@
 "use client";
 import React, { useState } from "react";
 import { StatSm, StatsSm } from "./Stats_sm";
+import { toggleBackgroundBlur } from "@/lib/utils";
+import DepositModal from "@/app/dashboard/components/DepositModal";
+import BorrowModal from "@/app/dashboard/components/BorrowModal";
+import WithdrawModal from "./WithdrawModal";
 
 type CardProps = {
   title: string;
   description: string;
   imageUrl: string;
-// add new prop
+  statsTitle: string;
+  statsValue: string;
 };
 
 const Card = ({
   title,
   description,
   imageUrl,
-  }: CardProps) => {
+  statsTitle,
+  statsValue,
+}: CardProps) => {
   const [isDepositModalOpen, setIsDepositModalOpen] = useState(false);
 
   const [isBorrowModalOpen, setIsBorrowModalOpen] = useState(false);
@@ -23,23 +30,43 @@ const Card = ({
 
   const handleOpenDepositModal = () => {
     setIsDepositModalOpen(true);
+    toggleBackgroundBlur(true); // Blur the background
   };
 
   const handleOpenBorrowModal = () => {
     setIsBorrowModalOpen(true);
+    toggleBackgroundBlur(true); // Blur the background
   };
 
   const handleOpenWithdrawModal = () => {
     setIsWithdrawModalOpen(true);
+    toggleBackgroundBlur(true); // Blur the background
   };
 
   return (
-    <div className="card w-96 border border-slate-500 rounded-xl ]">
-      
+    <div className="card w-96 border border-slate-500 rounded-xl hover:shadow-[0_35px_60px_-15px_rgba(20,241,149,0.3)]">
+      <DepositModal
+        title="Deposit"
+        isOpen={isDepositModalOpen}
+        onClose={() => setIsDepositModalOpen(false)}
+      />
+      <BorrowModal
+        title="Borrow Loan"
+        isOpen={isBorrowModalOpen}
+        onClose={() => setIsBorrowModalOpen(false)}
+      />
+      <WithdrawModal
+        title="Withdraw"
+        isOpen={isWithdrawModalOpen}
+        onClose={() => setIsWithdrawModalOpen(false)}
+      />
       <figure>
         <img src={imageUrl} alt="card" className="h-[200px] w-full" />
       </figure>
       <div className="card-body">
+        <StatsSm>
+          <StatSm title={statsTitle} value={statsValue} />
+        </StatsSm>
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
         <div className="card-actions justify-center mt-4">
