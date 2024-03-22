9dc0c6cf6d8b9cc9235f2a0f20732bec8ab2280e
Benjamin <tankerkzheng1@gmail.com>
2024-03-23 03:40:51 +0800

    feat: added components to repay page

--- src/app/repay/page.tsx
+++ src/app/repay/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { StatSm } from "./components/Stats_sm";
 
-const page = () => {
-  return <div>page</div>;
-};
+//add hooks here
 
-export default page;
+export default function Repay() {
+  const [isRepayModalOpen, setIsRepayModalOpen] = useState(false);
+
+  return (
+    <main className="flex flex-col items-center justify-between p-12">
+      <h1 className="font-bold text-2xl">Repay Your Debts</h1>
+
+      <div className="my-10 border rounded-xl border-slate-500 flex flex-col lg:flex-row p-10">
+        <StatSm title="Total Debts" value="$500,000" />
+        <StatSm title="Total Yield" value="$50,000" />
+      </div>
+
+      <div className="gap-4 flex flex-col lg:flex-row">
+        {/* add additional components here */}
+      </div>
+    </main>
+  );
+}
