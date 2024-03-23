2702b7593bec7b5e146c687b8ee353cc9c058558
Cheong Yee Chian <cheongyeechian@gmail.com>
2024-03-23 21:14:21 +0800

    deposit write contract function

--- src/app/dashboard/components/DepositDYSNModal.tsx
+++ src/app/dashboard/components/DepositDYSNModal.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import { toggleBackgroundBlur } from "../../../lib/utils";
 import { useVault } from "@/components/ContractInteraction";
 import { useContractRead, useContractWrite } from "@thirdweb-dev/react";
 import { BigNumber } from "ethers";
+
 
 type ModalProps = {
   title: string;
@@ -11,73 +12,79 @@ type ModalProps = {
   onClose: () => void;
 };
 
-const DepositDYSNModal = ({ title, isOpen, onClose }: ModalProps) => {
-  const handleDepositDYSNModalClose = () => {
+const DepositModal = ({ title, isOpen, onClose }: ModalProps) => {
+
+
+
+    const handleModalClose = () => {
     onClose();
     toggleBackgroundBlur(false); // Remove blur when modal closes
-  };
+    };
 
-  return (
+    return (
     <>
-      {isOpen && (
+        {isOpen && (
         <dialog
-          id="deposit_modal"
-          className="modal modal-middle sm:modal-middle"
-          open
+            id="deposit_modal"
+            className="modal modal-middle sm:modal-middle"
+            open
         >
-          <div className="modal-box dropshadow">
+            <div className="modal-box dropshadow">
             <h3 className="font-bold text-2xl">{title}</h3>
             <br />
 
             <div className="">
-              <label className="form-control md:max-w-lg">
+                <label className="form-control md:max-w-lg">
                 <div className="label">
-                  <span className="label-text font-bold">Amount</span>
+                    <span className="label-text font-bold">Amount</span>
                 </div>
                 <input
-                  type="number"
-                  placeholder="🪙 0.1"
-                  className="input input-bordered w-full lg:max-w-lg rounded-xl"
+                    type="number"
+                    placeholder="🪙 0.1"
+                    className="input input-bordered w-full lg:max-w-lg rounded-xl"
                 />
                 <div className="label">
-                  <span className="label-text-alt">${}</span>
-                  <span className="label-text-alt">Balance: {}</span>
+                    <span className="label-text-alt">${}</span>
+                    <span className="label-text-alt">Balance: {}</span>
                 </div>
-              </label>
+                </label>
             </div>
 
             <h1 className="font-bold mt-4">Transaction Details</h1>
             <div className="my-10 md:max-w-lg">
-              <div className="label">
+                <div className="label">
                 <span className="label-text-alt">🪙 APR</span>
                 <span className="label-text-alt">0</span>
-              </div>
-              <div className="label">
+                </div>
+                <div className="label">
                 <span className="label-text-alt">⛽ Transaction fees</span>
                 <span className="label-text-alt">{}</span>
-              </div>
+                </div>
             </div>
 
             <div className="modal-action flex flex-col items-center justify-center my-2 gap-2">
-              <button className="btn btn-primary rounded-xl w-48 hover:bg-white hover:border-white">
-                Deposit
-              </button>
-            </div>
-            <div className="modal-action flex flex-col items-center justify-center my-2 gap-2">
-              <form method="dialog">
                 <button
-                  onClick={handleDepositDYSNModalClose}
-                  className="btn btn-outline btn-error rounded-xl w-48"
-                >
-                  Close
-                </button>
-              </form>
-            </div>
-          </div>
-        </dialog>
-      )}
-    </>
-  );
-};
+                className="btn btn-primary rounded-xl w-48 hover:bg-white hover:border-white"
 
-export default DepositDYSNModal;
+                >
+                Deposit
+                </button>
+            </div>
+            <div className="modal-action flex flex-col items-center justify-center my-2 gap-2">
+                <form method="dialog">
+                <button
+                    onClick={handleModalClose}
+                    className="btn btn-outline btn-error rounded-xl w-48"
+                >
+                    Close
+                </button>
+                </form>
+            </div>
+            </div>
+        </dialog>
+        )}
+    </>
+    );
+    };
+
+    export default DepositModal;
